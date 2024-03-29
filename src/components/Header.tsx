de17e77aa1fd4441fdba6572462788fe1f3898ff
Willyan Costa <willyancr@gmail.com>
2024-03-28 23:30:01 -0300

    Update styles in Header and SideBar components
    Adjust styles in Header and SideBar components for better layout and spacing.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 function Header() {
   return (
     <nav className="flex justify-between w-full h-10 px-10 py-12">
-      <ul className="flex items-center gap-10 font-bold cursor-pointer">
+      <ul className="flex items-center gap-10 font-bold cursor-pointer text-xl">
         <li className='hover:text-white'>Filmes</li>
         <li className='hover:text-white'>Series</li>
         <li className='hover:text-white'>Animes</li>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -10,12 +10,12 @@ import {
 
 function SideBar() {
   return (
-    <div className="grid grid-rows-[100px_1fr_1fr] grid-flow-col w-48 bg-projeto-secondary border-r border-projeto-border/70 h-screen">
+    <div className="grid grid-rows-[100px_1fr_1fr] grid-flow-col w-60 bg-projeto-secondary border-r border-projeto-border/70 h-screen">
       <nav className="flex items-center gap-2 justify-center">
         <Clapperboard className="text-projeto-tertiary" />
         <h1 className="text-2xl text-white font-bold">FlixList</h1>
       </nav>
-      <nav className="flex flex-col gap-10 px-6 py-10 ">
+      <nav className="flex flex-col gap-10 px-4 py-10 ">
         <div className="flex items-center gap-3 hover:text-projeto-tertiary cursor-pointer">
           <Home size={22} />
           <p>Home</p>
@@ -29,7 +29,7 @@ function SideBar() {
           <p>Assistidos</p>
         </div>
       </nav>
-      <nav className="flex flex-col gap-8 px-6 py-10 mt-auto border-t border-projeto-border/70">
+      <nav className="flex flex-col gap-8 px-4 py-10 mt-auto border-t border-projeto-border/70">
         <div className="flex items-center gap-3 cursor-pointer">
           <Settings />
           <p>Configuração</p>
