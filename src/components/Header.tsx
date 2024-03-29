86a0d2ee9ce65b9b78bd6fa88893630001794af3
Willyan Costa <willyancr@gmail.com>
2024-03-28 23:25:12 -0300

    Add Header component with navigation and user avatar
    Create a new Header component with navigation links for "Filmes," "Series," and "Animes," along with a BellDot icon and user avatar.

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BellDot } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+function Header() {
+  return (
+    <nav className="flex justify-between w-full h-10 px-10 py-12">
+      <ul className="flex items-center gap-10 font-bold cursor-pointer">
+        <li className='hover:text-white'>Filmes</li>
+        <li className='hover:text-white'>Series</li>
+        <li className='hover:text-white'>Animes</li>
+      </ul>
+
+      <div className="flex items-center gap-3">
+        <BellDot />
+        <Avatar>
+          <AvatarImage src="https://github.com/willyancr.png" />
+        </Avatar>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
