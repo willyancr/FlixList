b5082c00bb50e321e26788a0057cc17f55ddd0c8
Willyan Costa <willyancr@gmail.com>
2024-04-01 20:15:44 -0300

    feat: Add MenuSidebar component to SideBar
    Create MenuSidebar component with props for title, selected state, onClick
    function, and icon. This component renders a clickable sidebar menu item.

---
+++ src/components/SideBar/MenuSidebar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type MenuSidebarProps = {
+  title: string;
+  selected: boolean;
+  onClick: () => void;
+  icon: React.ReactNode;
+};
+
+function MenuSidebar({ title, selected, icon, onClick }: MenuSidebarProps) {
+  return (
+    <div
+      className={`flex items-center gap-3 p-2 cursor-pointer hover:bg-projeto-tertiary/80 rounded ease-in duration-150 ${
+        selected ? 'bg-projeto-tertiary/80 rounded ease-in duration-150' : ''
+      }`}
+      onClick={onClick}
+    >
+      {icon}
+      <p>{title}</p>
+    </div>
+  );
+}
+
+export default MenuSidebar;
