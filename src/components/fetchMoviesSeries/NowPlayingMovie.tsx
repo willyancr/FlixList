d6415b9028192e27fbec7abcc0a2ceeb75d50f22
Willyan Costa <willyancr@gmail.com>
2024-04-02 19:08:24 -0300

    feat: Add NowPlayingMovie component for current movies display
    This commit introduces the NowPlayingMovie component, which utilizes the Swiper library to display the most recent movies fetched from the API. The component is designed to show a grid of movie posters, allowing users to visually browse through the current movies playing. The implementation includes pagination but sets it to non-clickable for this version. The movie posters are fetched using an environment variable for the image path, ensuring flexibility and ease of configuration. This addition enhances the user interface by providing a dynamic and visually appealing way to explore new movie releases.

---
+++ src/components/fetchMoviesSeries/NowPlayingMovie.tsx
@@ -0,0 +1,44 @@
+import { useData, MovieItem } from '../UserContext';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const movieIMG = import.meta.env.VITE_IMG;
+
+function NowPlayingMovie() {
+  const { nowPlayingMovie } = useData();
+  return (
+    <div>
+      <h2 className="border-b border-projeto-border/50 mb-6">
+        Os <span>{nowPlayingMovie?.length}</span> filmes mais recentes.
+      </h2>
+      <div className="grid grid-cols-5 gap-1">
+        <Swiper
+          slidesPerView={6}
+          spaceBetween={10}
+          pagination={{
+            clickable: false,
+          }}
+          modules={[Pagination]}
+          className="h-[200px] max-w-[740px] "
+        >
+          {nowPlayingMovie?.map((item: MovieItem) => (
+            <SwiperSlide
+              key={item.id}
+              // onClick={() => navigate(`/movie/${item.id}`)}
+            >
+              <img
+                src={`${movieIMG}${item.poster_path}`}
+                alt=""
+                className="rounded cursor-pointer hover:border border-blue-600"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default NowPlayingMovie;
