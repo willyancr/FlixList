6345fbbe1af0bb8a61858e4d701ddab3028644f0
Willyan Costa <willyancr@gmail.com>
2024-04-01 22:37:19 -0300

    style: Update Header component UI styles
    Remove AvatarFallback import and adjust spacing in Header component UI elements.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BellDot } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 
 function Header() {
   return (
-    <nav className="flex justify-between w-full h-10 px-10 py-12">
-      <ul className="flex items-center gap-10 font-bold cursor-pointer text-xl">
-        <li className='hover:text-white'>Filmes</li>
-        <li className='hover:text-white'>Series</li>
-        <li className='hover:text-white'>Animes</li>
+    <nav className="flex justify-between h-10 px-10 py-12 ">
+      <ul className="flex items-center gap-10 font-bold cursor-pointer text-xl ">
+        <li className="hover:text-white ">Filmes</li>
+        <li className="hover:text-white">Series</li>
+        <li className="hover:text-white">Animes</li>
       </ul>
 
       <div className="flex items-center gap-3">
