aba00b57a257250701715afe2f064ef502aaba64
Willyan Costa <willyancr@gmail.com>
2024-04-02 21:25:08 -0300

    feat: Add new routes for Movies, Series, and InfoMovie components
    Include new routes for Movies, Series, and InfoMovie components in the App component to enable navigation to these pages.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import SideBar from './components/SideBar/SideBar';
 import Watchlist from './components/Watchlist';
 import Home from './components/Home';
 import UserContextProvider from './components/UserContext';
+import InfoMovie from './components/InfoMovie';
+import Movies from './components/Header/Movies';
+import Series from './components/Header/Series';
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/infomovie/:id" element={<InfoMovie />} />
           </Routes>
         </BrowserRouter>
       </UserContextProvider>
