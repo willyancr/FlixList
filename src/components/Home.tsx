188105f2a97d06490e88e1a55dfb8d00bf89edc4
Willyan Costa <willyancr@gmail.com>
2024-04-02 22:55:23 -0300

    style: Update Home component with new navigation and UI changes
    - Import `useNavigate` from 'react-router-dom'
    - Update Header import path to './Header/Header'
    - Add new navigation functionality using `useNavigate`
    - Update UI elements and remove unused components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,10 @@
-import Header from './Header';
+import { useNavigate } from 'react-router-dom';
+import Header from './Header/Header';
 import { Input } from './ui/input';
 import { Search } from 'lucide-react';
-import TopMovies from './fetchMoviesSeries/TopMovies';
-import NowPlayingMovie from './fetchMoviesSeries/NowPlayingMovie';
-import UpComingMovie from './fetchMoviesSeries/UpComingMovie';
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col w-full">
       <Header />
@@ -17,9 +16,19 @@ function Home() {
             placeholder="Pesquise por um filme ou serie..."
           />
         </div>
-        <TopMovies />
-        <NowPlayingMovie />
-        <UpComingMovie />
+        <h2 className="border-b border-projeto-border/50 mb-6">
+          Resultado de um total de <span>0</span>
+        </h2>
+        <div className="grid grid-cols-2 gap-2">
+          <img
+            src="./src/image/capa-filme-01.png"
+            alt=""
+            className="rounded cursor-pointer hover:border border-blue-600 hover:scale-105 transition ease-linear duration-1000"
+            onClick={() => navigate('/infomovie')}
+          />
+
+          {/* <div>teste</div> */}
+        </div>
       </div>
     </div>
   );
