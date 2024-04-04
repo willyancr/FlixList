974ac6845c847066eef4f04f9460241771c73060
Willyan Costa <willyancr@gmail.com>
2024-04-03 23:07:31 -0300

    feat: Refactor Header component for better menu handling
    Extract menu logic into HandleMenus component for improved readability and maintainability. Utilize useData hook for state management.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,38 @@
 import { BellDot } from 'lucide-react';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { Link } from 'react-router-dom';
+import HandleMenus from '../HandleMenus';
+import { useData } from '../UserContext';
 
 function Header() {
+  const { selectedItem, handleClick } = useData();
+
   return (
     <header>
-      <nav className="flex justify-between h-10 px-10 py-12 ">
-        <ul className="flex items-center gap-10 font-bold cursor-pointer text-xl ">
-          <Link to="/movies">
-            <li className="hover:text-white ">Filmes</li>
+      <nav className="flex justify-between h-10 px-9 py-12">
+        <div className="flex items-center gap-5 font-bold cursor-pointer text-xl">
+          <Link to={'/filmes'} >
+            <HandleMenus
+              title="Filmes"
+              selected={selectedItem === 'filmes'}
+              onClick={() => handleClick('filmes')}
+            />
           </Link>
-          <Link to="/series">
-            <li className="hover:text-white">Series</li>
+          <Link to={'/series'}>
+            <HandleMenus
+              title="Series"
+              selected={selectedItem === 'series'}
+              onClick={() => handleClick('series')}
+            />
           </Link>
-          <li className="hover:text-white">Animes</li>
-        </ul>
-
+          <Link to={'/'}>
+            <HandleMenus
+              title="Animes"
+              selected={selectedItem === 'animes'}
+              onClick={() => handleClick('')}
+            />
+          </Link>
+        </div>
         <div className="flex items-center gap-3">
           <BellDot />
           <Avatar>
