4941c22c9acaea0e3d4a4c136304eb21e7246e38
Willyan Costa <willyancr@gmail.com>
2024-04-11 21:16:13 -0300

    Refactor Watchlist component for better movie display
    Import necessary dependencies and refactor movie display logic for improved
    functionality and readability. Fix onClick event and image source.

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -1,13 +1,11 @@
+import { Link } from 'react-router-dom';
+import { useDataMovie } from './Context/UserContextMovies';
 import Header from './Header/Header';
 
+const movieIMG = import.meta.env.VITE_IMG;
+
 function Watchlist() {
-  const capaFilme = [
-    {
-      id: Math.random(),
-      path: './src/image/capa-filme-01.png',
-    },
-  ];
-  const capaFilmeArray = Array(12).fill(capaFilme);
+  const { selectedPath, handleClickMovieSerie } = useDataMovie();
   return (
     <div className="flex flex-col w-full">
       <Header />
@@ -15,15 +13,17 @@ function Watchlist() {
         <h1 className="border-b border-projeto-border/50 mb-6">
           Você quer ver <span>40</span> filmes
         </h1>
-        <div className="grid grid-cols-5 gap-3">
-          {capaFilmeArray.map((item) => (
+        <div
+          className="grid grid-cols-5 gap-3"
+          onClick={() => handleClickMovieSerie(selectedPath.id)}
+        >
+          <Link to={`/infofilmes/${selectedPath.title}`}>
             <img
-              src={item[0].path}
+              src={`${movieIMG}${selectedPath.path}`}
               alt=""
-              key={item[0].id}
               className="rounded cursor-pointer hover:border border-blue-600 hover:scale-105 transition ease-linear duration-1000"
             />
-          ))}
+          </Link>
         </div>
       </div>
     </div>
