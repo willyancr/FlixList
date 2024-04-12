98761d7d74f38ec2865f39e780b5554c6141e722
Willyan Costa <willyancr@gmail.com>
2024-04-11 21:15:53 -0300

    Add onClick prop to VoteAndAddWatchlist component
    Add onClick prop to VoteAndAddWatchlist component for handling click events.

--- src/components/InfoMovieSerie/VoteAndAddWatchlist.tsx
+++ src/components/InfoMovieSerie/VoteAndAddWatchlist.tsx
@@ -2,14 +2,17 @@ function VoteAndAddWatchlist({
   title,
   className,
   icon,
+  onClick,
 }: {
   title: string | number;
   className?: string;
   icon?: React.ReactNode;
+  onClick?: () => void;
 }) {
   return (
     <div
       className={`${className} flex items-center gap-1 border border-projeto-tertiary rounded py-2 px-5 text-projeto-tertiary`}
+      onClick={onClick}
     >
       {icon}
       <h3>{title}</h3>
