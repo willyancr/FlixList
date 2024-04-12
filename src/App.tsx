b2de7656f0f9a59767f0b8565c525412bb9b2ccb
Willyan Costa <willyancr@gmail.com>
2024-04-11 21:16:03 -0300

    Refactor context providers in App component
    Move context providers for movies and series to wrap BrowserRouter in App component. This ensures proper context availability throughout the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import UserContextMovieProvider from './components/Context/UserContextMovies';
+import UserContextSerieProvider from './components/Context/UserContextSeries';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SideBar from './components/SideBar';
-import Watchlist from './components/Watchlist';
 import Home from './components/Home';
-import UserContextProvider from './components/UserContext';
-import InfoMovie from './components/InfoMovieSerie/InfoMovie';
+import Watchlist from './components/Watchlist';
 import Movies from './components/Header/Movies';
 import Series from './components/Header/Series';
+import InfoMovie from './components/InfoMovieSerie/InfoMovie';
 import InfoSerie from './components/InfoMovieSerie/InfoSerie';
 
 function App() {
   return (
     <div className="flex">
-      <UserContextProvider>
-        <BrowserRouter>
-          <SideBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/watchlist" element={<Watchlist />} />
-            <Route path="/filmes" element={<Movies />} />
-            <Route path="/series" element={<Series />} />
-            <Route path="/infofilmes/:id" element={<InfoMovie />} />
-            <Route path="/infoseries/:id" element={<InfoSerie />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContextProvider>
+      <UserContextMovieProvider>
+        <UserContextSerieProvider>
+          <BrowserRouter>
+            <SideBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/watchlist" element={<Watchlist />} />
+              <Route path="/filmes" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/infofilmes/:id" element={<InfoMovie />} />
+              <Route path="/infoseries/:id" element={<InfoSerie />} />
+            </Routes>
+          </BrowserRouter>
+        </UserContextSerieProvider>
+      </UserContextMovieProvider>
     </div>
   );
 }
